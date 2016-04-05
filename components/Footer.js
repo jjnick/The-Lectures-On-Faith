bb4dcfdbd62b48b989bf775088dc077a00314a63
Josh Nicholls <josh.nicholls@outlook.com>
2016-04-05 06:35:45 -0500

    Added footer content and updated stylesheet
    In addition to adding content to the footer a new production bundle was
    created to update thelecturesonfaith.com.

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,10 @@ import React from 'react';
 class Footer extends React.Component {
     render () {
         return (
-            <div>
-                footer here.
+            <div className="text-center">
+                This site www.thelecturesonfaith.com is not affiliated with The Church of Jesus Christ of Latter-day Saints.
+                <br />
+                Information on The Church of Jesus Christ of Latter-day Saints can be found at <a href="https://www.lds.org/">lds.org</a> or <a href="https://www.mormon.org/">mormon.org</a>
             </div>
         );
     }
